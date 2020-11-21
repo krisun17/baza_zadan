425fd60c0d48037d1dba333b548893074032946c
krisun17 <krisun17@gmail.com>
2020-11-21 18:08:29 +0100

    add biblio

--- tbconfig.py
+++ tbconfig.py
@@ -16,7 +16,9 @@ class Config(object):
                                      r"\usepackage{float}",
                                      r"\newgeometry{tmargin=1.5cm, bmargin=1.5cm, lmargin=1.5cm, rmargin=1.5cm}",
                                      r"\begin{document}"],
-                    "end_text": [r"\end{document}"]}
+                    "end_text": [r"\bibliography{../biblio}", 
+                                 r"\bibliographystyle{plain}",
+                                 r"\end{document}"]}
     tasks_config_file = os.path.join(root_dir, "tasks_config.json")
     sections_config_file = os.path.join(root_dir, "sections_config.json")
     tsk_ext = ".tex"
